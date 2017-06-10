ae2027f143f0d06568d2d5a3ea64273e26b61bf5
ALAN <ALAN@AKN-PC>
2017-06-10 18:18:23 +0200

    Admin glitch fixed

--- NutriCity/AboutUs.Master.designer.cs
+++ NutriCity/AboutUs.Master.designer.cs
@@ -13,6 +13,15 @@ namespace NutriCity {
     public partial class AboutUs {
         
         /// <summary>
+        /// adminLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor adminLink;
+        
+        /// <summary>
         /// cartCount control.
         /// </summary>
         /// <remarks>
