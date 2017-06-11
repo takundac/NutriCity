3e9434aec91e258f67891080f2f22e241bbab5b2
Nasiha Ally <allynasiha82@gmail.com>
2017-06-11 16:41:53 +0200

    removed datagrid

--- NutriCity/ProductList.aspx.designer.cs
+++ NutriCity/ProductList.aspx.designer.cs
@@ -13,33 +13,6 @@ namespace NutriCity {
     public partial class ProductList {
         
         /// <summary>
-        /// TextBox1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
-        
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
-        
-        /// <summary>
-        /// GridView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
-        
-        /// <summary>
         /// productList control.
         /// </summary>
         /// <remarks>
